9081eb62914f3f6c960f1686df159e1c729790f7
mansouri youssef <mansouriyoussef1991@gmail.com>
2022-06-26 13:53:07 +0100

    update vlume jenkins to use docker

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="javascript:void(0)">
           I18n
         </a>
         <button
